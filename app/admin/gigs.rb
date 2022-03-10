aab826a5bc1e4e4b690261a32e35a7f74243054f
anonymous anonymous <savg94@gmail.com>
2022-03-10 06:31:51 +0000

    Whitelist columns in Gig for ActiveAdmin

--- app/admin/gigs.rb
+++ app/admin/gigs.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Gig do
 
+ permit_params :request_id, :provider_id, :status
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
