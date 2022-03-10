505cef15306a2890bbb298a788d9af5cc7b709a7
anonymous anonymous <savg94@gmail.com>
2022-03-10 06:31:40 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "requests#index"
   resources :chats
   resources :gigs
