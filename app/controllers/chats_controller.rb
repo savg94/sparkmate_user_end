89896eec472049ef1240adfd5673151e33f515ba
anonymous anonymous <savg94@gmail.com>
2022-03-10 06:32:30 +0000

    Only chat user can modify chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,6 @@
 class ChatsController < ApplicationController
+  before_action :current_user_must_be_chat_user, only: [:edit, :update, :destroy] 
+
   before_action :set_chat, only: [:show, :edit, :update, :destroy]
 
   # GET /chats
@@ -57,6 +59,14 @@ class ChatsController < ApplicationController
 
 
   private
+
+  def current_user_must_be_chat_user
+    set_chat
+    unless current_user == @chat.user
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_chat
       @chat = Chat.find(params[:id])
