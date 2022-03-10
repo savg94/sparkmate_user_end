b4f542dd2c8da1ec3e44560776768cc0d87d64cd
anonymous anonymous <savg94@gmail.com>
2022-03-10 06:31:26 +0000

    Direct association between users and requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :requests,
+             :dependent => :nullify
+
   has_many   :chats,
              :dependent => :nullify
 
